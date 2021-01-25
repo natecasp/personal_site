4a7d720f757b606c92ccc85bc5a7cef826af5709
natecasp <77999537+natecasp@users.noreply.github.com>
2021-01-25 17:32:43 -0600

    Add files via upload

---
+++ Personal Site/animation.js
@@ -0,0 +1,6 @@
+document.addEventListener("scroll", function(event){
+    scroll = this.scrollY;
+    if(scroll > 0) {
+        document.getElementsByClassName("logo").style.transform = "translate(.2,.2)";
+    }
+});
